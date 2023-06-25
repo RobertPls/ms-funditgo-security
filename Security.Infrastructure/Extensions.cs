2d7703af76bddbc9d48b3c9f9b8232d8ec195ea7
Robert <robertperezlino@gmail.com>
2023-06-25 18:42:50 -0400

    Primer Commit

---
+++ Security.Infrastructure/Extensions.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using Security.Application.Services;
+using Security.Infrastructure.EntityFramework;
+using Security.Infrastructure.Security;
+using Security.Infrastructure.Services;
+using System.Text;
+
+namespace Security.Infrastructure
+{
+    public static class Extensions
+    {
+        public static void AddSecurity(IServiceCollection services, IConfiguration configuration)
+        {
+
+            services.AddIdentity<ApplicationUser, ApplicationRole>(options =>
+            {
+                options.Password.RequiredLength = 8;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(10);
+                options.Lockout.MaxFailedAccessAttempts = 5;
+            }).AddEntityFrameworkStores<SecurityDbContext>()
+            .AddDefaultTokenProviders();
+
+            services.AddAuthorization(config =>
+            {
+                var defaultAuthBuilder = new AuthorizationPolicyBuilder();
+                var defaultAuthPolicy = defaultAuthBuilder
+                    .RequireAuthenticatedUser()
+                    .Build();
+
+                config.DefaultPolicy = defaultAuthPolicy;
+
+                foreach (var mnemonic in ApplicationPermission.GetAllPermissions().Select(x => x.Mnemonic))
+                {
+                    config.AddPolicy(mnemonic,
+                        policy => policy.RequireClaim("Permission", new string[] { mnemonic }));
+                }
+            });
+
+            JwtOptions jwtoptions = configuration.GetSection(JwtOptions.SectionName).Get<JwtOptions>();
+
+            services.AddAuthentication().AddJwtBearer("Bearer", jwtOptions =>
+            {
+                var signingKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtoptions.SecretKey));
+                jwtOptions.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    IssuerSigningKey = signingKey,
+                    ValidateIssuer = jwtoptions.ValidateIssuer,
+                    ValidateAudience = jwtoptions.ValidateAudience,
+                    ValidIssuer = jwtoptions.ValidIssuer,
+                    ValidAudience = jwtoptions.ValidAudience
+                };
+            });
+            System.Diagnostics.Debug.WriteLine(jwtoptions==null?true:false);
+            services.AddSingleton(jwtoptions);
+
+
+            services.AddScoped<SecurityInitializer>();
+            services.AddScoped<ISecurityService, SecurityService>();
+
+        }
+
+        public static void AddDatabase(IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("DbConnectionString");
+
+            services.AddDbContext<SecurityDbContext>(ctx =>
+                ctx.UseSqlServer(connectionString));
+
+
+            services.AddHostedService<DbInitializer>();
+        }
+
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+
+            AddDatabase(services, configuration);
+            AddSecurity(services, configuration);
+
+            return services;
+        }
+    }
+}
