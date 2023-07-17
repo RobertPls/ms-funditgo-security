22edf2dd0e5fe0b16c2fae9a9028b5094b6db868
Robert <robertperezlino@gmail.com>
2023-07-16 20:41:19 -0400

    v1

--- Shared/IntegrationEvents/ProyectoCreado.cs
+++ Shared/IntegrationEvents/ProyectoCreado.cs
@@ -5,8 +5,12 @@ namespace Shared.IntegrationEvents
     public record ProyectoCreado : IntegrationEvent
     {
         public Guid ProyectoId { get; set; }
+
+        public Guid CreadorId { get; set; }
+
+        public Guid TipoProyectoId { get; set; }
+
         public string Titulo { get; set; }
-
-
+        public string Estado { get; set; }
     }
 }
